fe178e810f8633722e8ebb3d10e17c97a537e2e7
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2016-02-26 17:10:03 +0000

    Init Flask

--- example.py
+++ example.py
@@ -1,4 +1,21 @@
+import datetime
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+# ToDo:
 # Flask
 # & serve static
 # Takes a document (file/url)
 # 1 View that renders the document
+
+
+@app.route('/')
+def docs():
+    now = datetime.datetime.now()
+    return render_template('coreapi_docs/templates/home.html', date=now)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
