2586b6a9edd0bd79213f7b91e8c97c17959ae7bd
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2016-02-26 21:02:04 +0000

    List tags, subdocs with description & fields

--- coreapi_docs/docs.py
+++ coreapi_docs/docs.py
@@ -9,26 +9,4 @@ class Docs(object):
         self.document = codec.load(content)
 
     def get_docs(self):
-        print '# %s' % self.document.title
-        print
-
-        for tag, sub_doc in self.document.data.items():
-            print '## %s' % tag
-            print
-
-            for name, link in sub_doc.links.items():
-                print '### %s' % name
-                print
-                print link.description
-                print
-                for field in link.fields:
-                    if field.required:
-                        print '* %s - %s' % (field.name, field.description)
-                    else:
-                        print '* [%s] - %s' % (field.name, field.description)
-                print
-
-            print '---'
-            print
-
         return self.document
