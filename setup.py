cb67edf96b6b293952b91fab065f0b082ec088d4
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2016-02-28 23:08:49 +0000

    Init Setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='coreapidocs',
+    version=__import__('coreapidocs').__version__,
+    author="Emmanouil Konstantinidis",
+    author_email="manos@iamemmanouil.com",
+    packages=find_packages(),
+    include_package_data=True,
+    url="https://github.com/ekonstantinidis/coreapi-docs",
+    license='BSD',
+    description="Document APIs with CoreAPI.",
+    long_description=open("README.md").read(),
+    install_requires=[
+        'Flask',
+        'Jinja2',
+        'coreapi'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ],
+)
